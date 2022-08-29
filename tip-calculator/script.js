99ace40cdf931567c489b0be0c313bc6aed60ce9
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-08-28 22:24:15 -0400

    tip calculator

---
+++ tip-calculator/script.js
@@ -0,0 +1,94 @@
+const calculatorDisplay = document.querySelector("h1");
+const inputBtns = document.querySelectorAll("button");
+const clearBtn = document.getElementById("clear-btn");
+const tipsBtns = document.querySelectorAll(".tip");
+
+let firstValue = 0;
+let operatorValue = "";
+let awaitingNextValue = false;
+let tipsValue = 0;
+
+function sendNumberValue(number) {
+  // Replace current display value iif first value is entered
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    // If current display value is 0, replace it, else if not add number
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent =
+      displayValue === "0" ? number : displayValue + number;
+  }
+}
+
+function addDecimal() {
+  // If operator pressed, don't add decimal
+  if (awaitingNextValue) return;
+  // If no decimal, add one
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+}
+
+//  Calculate 1st & 2nd values dependingon operator
+const calculate = {
+  "/": (firstNumber, secondNumber) => firstNumber / secondNumber,
+  "*": (firstNumber, secondNumber) => firstNumber * secondNumber,
+  "+": (firstNumber, secondNumber) => firstNumber + secondNumber,
+  "-": (firstNumber, secondNumber) => firstNumber - secondNumber,
+  "=": (firstNumber, secondNumber) => secondNumber,
+};
+
+function useOperator(operator) {
+  const currentValue = Number(calculatorDisplay.textContent);
+  // Prevent multiple operators
+  if (operatorValue && awaitingNextValue) {
+    operatorValue = operator;
+    return;
+  }
+  // Assign firstValue is no value exists
+  if (!firstValue) {
+    firstValue = currentValue;
+  } else {
+    const calculation = calculate[operatorValue](firstValue, currentValue);
+    calculatorDisplay.textContent = calculation;
+    firstValue = calculation;
+  }
+  // Ready for next value, store operator
+  awaitingNextValue = true;
+  operatorValue = operator;
+}
+
+// Add Event Listeners for numbers, operators, decimal buttons
+inputBtns.forEach((inputBtn) => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains("operator")) {
+    inputBtn.addEventListener("click", () => useOperator(inputBtn.value));
+  } else if (inputBtn.classList.contains("decimal")) {
+    inputBtn.addEventListener("click", () => addDecimal());
+  }
+});
+
+// Reset all vales, display
+function resetAll() {
+  firstValue = 0;
+  operatorValue = "";
+  awaitingNextValue = false;
+  calculatorDisplay.textContent = "0";
+}
+
+//calculate tips
+function calculateTips(percentage) {
+  tipsValue = (Number(calculatorDisplay.textContent) * (1 + percentage)) / 100;
+  calculatorDisplay.textContent = tipsValue;
+}
+
+// Event Listener - TRY ADDING KEYBOARD INPUT FEATURE
+clearBtn.addEventListener("click", resetAll);
+tipsBtns.forEach((tipsBtn) => {
+  tipsBtn.addEventListener("click", () => calculateTips(tipsBtn.value));
+});
+// document.addEventListener("keydown", (e) => {
+//   console.log(e);
+// });
